64c1e30c6ae872f3369e29928f115a307002f69d
Jlobblet <jlobblet@gmx.com>
2020-05-03 15:27:00 +0100

    Initial upload

---
+++ Sub-Wrecker.py
@@ -0,0 +1,60 @@
+import argparse
+import gzip
+from os import path
+import xml.etree.ElementTree as ET
+
+from IDENTIFIERS import IDENTIFIERS
+
+parser = argparse.ArgumentParser(description="Wreck all possible things that can be wrecked on a submarine in Barotrauma.")
+parser.add_argument("files", nargs="+", type=str, help="The files to be converted.")
+parser.add_argument("-i", "--inplace", action="store_true")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    for file in args.files:
+        print(f"Attempting to wreck {file}...")
+        if not path.isfile(file):
+            # Ignore non-files
+            print(f"Cannot find {file}, skipping.")
+            continue
+        
+        splitext = path.splitext(file)
+        filename = splitext[0]
+        extension = splitext[-1].lower()
+        # Only work on .subs and .xmls for safety
+        if extension == ".sub":
+            with gzip.open(file, "rb") as sub:
+                sub_xml = sub.read()
+        elif extension == ".xml":
+            with open(file, "r") as sub:
+                sub_xml = sub.read()
+        else:
+            print(f"File {file} has unsupported file extension, skipping.")
+            continue
+        
+        # Run the conversions
+        print(f"Wrecking {file}...")
+        tree = ET.fromstring(sub_xml)
+        for node in tree.iter():
+            value = str(node.attrib.get("identifier", ""))
+            if value in IDENTIFIERS:
+                # print(f"Wrecking {value} ==> {IDENTIFIERS[value]}")
+                node.attrib["identifier"] = IDENTIFIERS[value]
+        # Convert back to a string for writing to file
+        wrecked_xml = ET.tostring(tree)
+        
+        # If not inplace, make a " - Wrecked" suffix
+        if not args.inplace:
+            out_file = f"{filename} - Wrecked{extension}"
+        else:
+            out_file = file
+        
+        print(f"Saving to {out_file}...")
+        # Save the submarine to the file we decided on
+        if extension == ".sub":
+            with gzip.open(out_file, "wb+") as sub:
+                sub.write(wrecked_xml)
+        else:
+            with open(out_file, "wb+") as sub:
+                sub.write(wrecked_xml)
+        print(f"Completed {file}.")
