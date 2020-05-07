a541317213e4e4fc7f9c7e461c7d2b08b16cdfe6
Jlobblet <jlobblet@gmx.com>
2020-05-07 13:13:07 +0100

    Encapsulate variables in Data.cs

--- Sub-Wrecker/Sub-Wrecker/Data.cs
+++ Sub-Wrecker/Sub-Wrecker/Data.cs
@@ -7,32 +7,45 @@ namespace Sub_Wrecker
 {
     internal class Data
     {
-        public static Dictionary<string, string> Identifiers = XElement
-            .Parse(File.ReadAllText("Resources/Identifiers.xml"))
-            .Elements()
-            .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
-
-        public static Dictionary<string, string> ContainerTags = XElement
+        private static Dictionary<string, string> containerTags = XElement
             .Parse(File.ReadAllText("Resources/ContainerTags.xml"))
             .Elements()
             .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
 
-        public static List<string> SignalComponents = XElement
-            .Parse(File.ReadAllText("Resources/SignalComponents.xml"))
+        private static List<string> doors = XElement
+            .Parse(File.ReadAllText("Resources/Doors.xml"))
             .Descendants()
             .Select(XElement => XElement.Name.ToString())
             .ToList();
 
-        public static List<string> Wires = XElement
+        private static Dictionary<string, string> identifiers = XElement
+            .Parse(File.ReadAllText("Resources/Identifiers.xml"))
+            .Elements()
+            .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
+
+        private static List<string> lights = XElement
+            .Parse(File.ReadAllText("Resources/Lights.xml"))
+            .Elements()
+            .Select(XElement => XElement.Name.ToString())
+            .ToList();
+
+        private static List<string> signalComponents = XElement
+           .Parse(File.ReadAllText("Resources/SignalComponents.xml"))
+           .Descendants()
+           .Select(XElement => XElement.Name.ToString())
+           .ToList();
+
+        private static List<string> wires = XElement
             .Parse(File.ReadAllText("Resources/Wires.xml"))
             .Descendants()
             .Select(XElement => XElement.Name.ToString())
             .ToList();
 
-        public static List<string> Doors = XElement
-            .Parse(File.ReadAllText("Resources/Doors.xml"))
-            .Descendants()
-            .Select(XElement => XElement.Name.ToString())
-            .ToList();
+        public static Dictionary<string, string> ContainerTags { get => containerTags; set => containerTags = value; }
+        public static List<string> Doors { get => doors; set => doors = value; }
+        public static Dictionary<string, string> Identifiers { get => identifiers; set => identifiers = value; }
+        public static List<string> Lights { get => lights; set => lights = value; }
+        public static List<string> SignalComponents { get => signalComponents; set => signalComponents = value; }
+        public static List<string> Wires { get => wires; set => wires = value; }
     }
 }
