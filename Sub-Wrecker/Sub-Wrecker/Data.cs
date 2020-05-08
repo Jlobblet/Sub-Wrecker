be63206470513b098e73439b24c19977d5038691
Jlobblet <jlobblet@gmx.com>
2020-05-08 15:09:38 +0100

    Make values in Data.cs readonly

--- Sub-Wrecker/Sub-Wrecker/Data.cs
+++ Sub-Wrecker/Sub-Wrecker/Data.cs
@@ -7,45 +7,45 @@ namespace Sub_Wrecker
 {
     internal class Data
     {
-        private static Dictionary<string, string> containerTags = XElement
+        private readonly static Dictionary<string, string> containerTags = XElement
             .Parse(File.ReadAllText("Resources/ContainerTags.xml"))
             .Elements()
             .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
 
-        private static List<string> doors = XElement
+        private readonly static List<string> doors = XElement
             .Parse(File.ReadAllText("Resources/Doors.xml"))
             .Descendants()
             .Select(XElement => XElement.Name.ToString())
             .ToList();
 
-        private static Dictionary<string, string> identifiers = XElement
+        private readonly static Dictionary<string, string> identifiers = XElement
             .Parse(File.ReadAllText("Resources/Identifiers.xml"))
             .Elements()
             .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
 
-        private static List<string> lights = XElement
+        private readonly static List<string> lights = XElement
             .Parse(File.ReadAllText("Resources/Lights.xml"))
             .Elements()
             .Select(XElement => XElement.Name.ToString())
             .ToList();
 
-        private static List<string> signalComponents = XElement
+        private readonly static List<string> signalComponents = XElement
            .Parse(File.ReadAllText("Resources/SignalComponents.xml"))
            .Descendants()
            .Select(XElement => XElement.Name.ToString())
            .ToList();
 
-        private static List<string> wires = XElement
+        private readonly static List<string> wires = XElement
             .Parse(File.ReadAllText("Resources/Wires.xml"))
             .Descendants()
             .Select(XElement => XElement.Name.ToString())
             .ToList();
 
-        public static Dictionary<string, string> ContainerTags { get => containerTags; set => containerTags = value; }
-        public static List<string> Doors { get => doors; set => doors = value; }
-        public static Dictionary<string, string> Identifiers { get => identifiers; set => identifiers = value; }
-        public static List<string> Lights { get => lights; set => lights = value; }
-        public static List<string> SignalComponents { get => signalComponents; set => signalComponents = value; }
-        public static List<string> Wires { get => wires; set => wires = value; }
+        public static Dictionary<string, string> ContainerTags { get => containerTags; }
+        public static List<string> Doors { get => doors; }
+        public static Dictionary<string, string> Identifiers { get => identifiers; }
+        public static List<string> Lights { get => lights; }
+        public static List<string> SignalComponents { get => signalComponents; }
+        public static List<string> Wires { get => wires; }
     }
 }
