1e09f34f6c76166c59f1521d638f7cfb7db10426
Jlobblet <jlobblet@gmx.com>
2020-10-20 23:49:44 +0100

    Refactor Data.cs

--- Sub-Wrecker/Sub-Wrecker/Data.cs
+++ Sub-Wrecker/Sub-Wrecker/Data.cs
@@ -5,54 +5,57 @@ using System.Xml.Linq;
 
 namespace Sub_Wrecker
 {
-    internal class Data
+    internal static class Data
     {
-        private readonly static Dictionary<string, string> containerTags = XElement
-            .Parse(File.ReadAllText("Resources/ContainerTags.xml"))
-            .Elements()
-            .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
+        static Data()
+        {
+            Doors = XElement
+                    .Parse(File.ReadAllText("Resources/Doors.xml"))
+                    .Descendants()
+                    .Select(elt => elt.Name.ToString())
+                    .ToList();
+            
+            ContainerTags = XElement
+                            .Parse(File.ReadAllText("Resources/ContainerTags.xml"))
+                            .Elements()
+                            .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
+            
+            Identifiers = XElement
+                          .Parse(File.ReadAllText("Resources/Identifiers.xml"))
+                          .Elements()
+                          .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
+            
+            Lights = XElement
+                        .Parse(File.ReadAllText("Resources/Lights.xml"))
+                        .Elements()
+                        .Select(elt => elt.Name.ToString())
+                        .ToList();
+            
+            SignalComponents = XElement
+                               .Parse(File.ReadAllText("Resources/SignalComponents.xml"))
+                               .Descendants()
+                               .Select(elt => elt.Name.ToString())
+                               .ToList();
+            
+            Wires = XElement
+                    .Parse(File.ReadAllText("Resources/Wires.xml"))
+                    .Descendants()
+                    .Select(elt => elt.Name.ToString())
+                    .ToList();
+            
+            Condition = XElement
+                        .Parse(File.ReadAllText("Resources/Condition.xml"))
+                        .Descendants()
+                        .Select(elt => elt.Name.ToString())
+                        .ToList();
+        }
 
-        private readonly static List<string> doors = XElement
-            .Parse(File.ReadAllText("Resources/Doors.xml"))
-            .Descendants()
-            .Select(XElement => XElement.Name.ToString())
-            .ToList();
-
-        private readonly static Dictionary<string, string> identifiers = XElement
-            .Parse(File.ReadAllText("Resources/Identifiers.xml"))
-            .Elements()
-            .ToDictionary(k => k.Name.ToString(), v => v.Value.ToString());
-
-        private readonly static List<string> lights = XElement
-            .Parse(File.ReadAllText("Resources/Lights.xml"))
-            .Elements()
-            .Select(XElement => XElement.Name.ToString())
-            .ToList();
-
-        private readonly static List<string> signalComponents = XElement
-           .Parse(File.ReadAllText("Resources/SignalComponents.xml"))
-           .Descendants()
-           .Select(XElement => XElement.Name.ToString())
-           .ToList();
-
-        private readonly static List<string> wires = XElement
-            .Parse(File.ReadAllText("Resources/Wires.xml"))
-            .Descendants()
-            .Select(XElement => XElement.Name.ToString())
-            .ToList();
-
-        private readonly static List<string> condition = XElement
-            .Parse(File.ReadAllText("Resources/Condition.xml"))
-            .Descendants()
-            .Select(XElement => XElement.Name.ToString())
-            .ToList();
-
-        public static Dictionary<string, string> ContainerTags { get => containerTags; }
-        public static List<string> Doors { get => doors; }
-        public static Dictionary<string, string> Identifiers { get => identifiers; }
-        public static List<string> Lights { get => lights; }
-        public static List<string> SignalComponents { get => signalComponents; }
-        public static List<string> Wires { get => wires; }
-        public static List<string> Condition { get => condition; }
+        public static Dictionary<string, string> ContainerTags { get; }
+        public static List<string> Doors { get; }
+        public static Dictionary<string, string> Identifiers { get; }
+        public static List<string> Lights { get; }
+        public static List<string> SignalComponents { get; }
+        public static List<string> Wires { get; }
+        public static List<string> Condition { get; }
     }
 }
