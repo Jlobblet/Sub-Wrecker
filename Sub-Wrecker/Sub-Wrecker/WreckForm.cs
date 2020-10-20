0c6e1d8521a5af96b1bf8dd2691409927b9d1f2c
Jlobblet <jlobblet@gmx.com>
2020-10-20 23:53:44 +0100

    Refactor WreckForm.cs

--- Sub-Wrecker/Sub-Wrecker/WreckForm.cs
+++ Sub-Wrecker/Sub-Wrecker/WreckForm.cs
@@ -5,13 +5,15 @@ using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using BaroLib;
+// ReSharper disable LocalizableElement
 
 namespace Sub_Wrecker
 {
     public partial class WreckForm : Form
     {
-        private readonly string DefaultOpenLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Steam", "steamapps", "common", "Barotrauma");
-        protected IEnumerable<string> SubFilePaths;
+        private readonly string defaultOpenLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Steam", "steamapps", "common", "Barotrauma");
+        private IEnumerable<string> subFilePaths;
         public WreckForm()
         {
             InitializeComponent();
@@ -31,83 +33,73 @@ namespace Sub_Wrecker
 
         private void BrowseButton_Click(object sender, EventArgs e)
         {
-            CommonOpenFileDialog dialog = new CommonOpenFileDialog()
+            var dialog = new CommonOpenFileDialog()
+                         {
+                             InitialDirectory = defaultOpenLocation,
+
+                             Multiselect = true,
+
+                             EnsureFileExists = true,
+                             EnsurePathExists = true,
+
+                             DefaultExtension = "save",
+
+                             ShowHiddenItems = true,
+                         };
+
+            if (dialog.ShowDialog() != CommonFileDialogResult.Ok) return;
+            subFilePaths = dialog.FileNames.ToList();
+            int length = subFilePaths.Count();
+            string filePathBox;
+            if (length > 1)
             {
-                InitialDirectory = DefaultOpenLocation,
-
-                Multiselect = true,
-
-                EnsureFileExists = true,
-                EnsurePathExists = true,
-
-                DefaultExtension = "save",
-
-                ShowHiddenItems = true,
-            };
-
-            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
-            {
-                SubFilePaths = dialog.FileNames;
-                int length = SubFilePaths.Count();
-                string filePathBox;
-                if (length > 1)
+                filePathBox = "";
+                for (var i = 0; i < length; i++)
                 {
-                    filePathBox = "";
-                    for (int i = 0; i < length; i++)
+                    filePathBox += $"\"{subFilePaths.ElementAt(i)}\"";
+                    if (i + 1 != length)
                     {
-                        filePathBox += "\"" + SubFilePaths.ElementAt(i) + "\"";
-                        if (i + 1 != length)
-                        {
-                            filePathBox += ",";
-                        }
+                        filePathBox += ",";
                     }
                 }
-                else
-                {
-                    filePathBox = SubFilePaths.ElementAt(0);
-                }
-                FilePathBox.Text = filePathBox;
-
             }
+            else
+            {
+                filePathBox = subFilePaths.ElementAt(0);
+            }
+            FilePathBox.Text = filePathBox;
         }
 
         private void WreckButton_Click(object sender, EventArgs e)
         {
-            foreach (string fileName in SubFilePaths)
+            var settings = new WreckerSettings(
+                                               ConditionCheckBox.Checked,
+                                               ContainerTagsCheckBox.Checked,
+                                               DeleteComponentsCheckBox.Checked,
+                                               DeleteWiresCheckBox.Checked,
+                                               DoorBehaviourComboBox.SelectedIndex,
+                                               InplaceCheckBox.Checked,
+                                               LightingShadowsCheckBox.Checked,
+                                               LightingTurnOffCheckBox.Checked,
+                                               PreserveColourCheckBox.Checked,
+                                               RenameCheckBox.Checked,
+                                               SpawnpointComboBox.SelectedIndex);
+            foreach (string fileName in subFilePaths)
             {
                 string extension = Path.GetExtension(fileName);
                 if (extension != ".sub")
                 {
-                    Console.WriteLine("File " + fileName + " has an unsupported extension, skipping.");
+                    Console.WriteLine($"File {fileName} has an unsupported extension, skipping.");
                     continue;
                 }
-                Console.WriteLine("Loading " + fileName + "...");
-                XDocument sub = SaveUtils.LoadSub(fileName);
+                Console.WriteLine($"Loading {fileName}...");
+                XDocument sub = IoUtil.LoadSub(fileName);
                 Console.WriteLine("...loaded.");
-                WreckerSettings settings = new WreckerSettings(
-                    conditionTo0: ConditionCheckBox.Checked,
-                    containerTags: ContainerTagsCheckBox.Checked,
-                    deleteComponents: DeleteComponentsCheckBox.Checked,
-                    deleteWires: DeleteWiresCheckBox.Checked,
-                    doorBehaviour: DoorBehaviourComboBox.SelectedIndex,
-                    inplace: InplaceCheckBox.Checked,
-                    lightingShadows: LightingShadowsCheckBox.Checked,
-                    lightingTurnOff: LightingTurnOffCheckBox.Checked,
-                    preserveColour: PreserveColourCheckBox.Checked,
-                    renameSub: RenameCheckBox.Checked,
-                    spawnpointBehaviour: SpawnpointComboBox.SelectedIndex);
                 XDocument wreckedSub = Wrecker.Wreck_Sub(sub, settings);
-                string outFileName;
-                if (InplaceCheckBox.Checked)
-                {
-                    outFileName = fileName;
-                }
-                else
-                {
-                    outFileName = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName) + "_Wrecked.sub");
-                }
-                Console.WriteLine("Saving to " + outFileName + "...");
-                SaveUtils.SaveSub(wreckedSub, outFileName);
+                string outFileName = InplaceCheckBox.Checked ? fileName : Path.Combine(Path.GetDirectoryName(fileName) ?? "",
+                                         $"{Path.GetFileNameWithoutExtension(fileName)}_Wrecked.sub");
+                Console.WriteLine($"Saving to {outFileName}...");
+                wreckedSub.SaveSub(outFileName);
                 Console.WriteLine("...saved.");
             }
         }
